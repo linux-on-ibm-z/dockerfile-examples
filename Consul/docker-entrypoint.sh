1ff2ef55c4aea1c303d95c48b363f9cc5dea4474
aborkar-ibm <aborkar@us.ibm.com>
2020-10-20 15:09:57 +0530

    Update docker-entrypoint.sh

--- Consul/docker-entrypoint.sh
+++ Consul/docker-entrypoint.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/bash
+#!/usr/bin/dumb-init /bin/sh
 set -e
-
 # Note above that we run dumb-init as PID 1 in order to reap zombie processes
 # as well as forward signals to all processes in its session. Normally, sh
 # wouldn't do either of these functions so we'd leak zombies as well as do
 # unclean termination of all our sub-processes.
-#As of docker 1.13, using docker run --init achieves the same outcome.
-
+# As of docker 1.13, using docker run --init achieves the same outcome.
 # You can set CONSUL_BIND_INTERFACE to the name of the interface you'd like to
 # bind to and this will look up the IP and pass the proper -bind= option along
 # to Consul.
